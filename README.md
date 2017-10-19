e5e39a37dcfb89552813f3158c208e513ea24d9e
Sem <polyismstudio@gmail.com>
2017-10-19 14:57:10 +0200

    generic

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 ### TODO
 1. Add password checking (firebase requires a password to be at least 6 characters)
+2. Add browser warning if the user doesnt support svg format. (this is required by the quill editor)
--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,59 @@
-var fileinclude = require('gulp-file-include'),
-  gulp = require('gulp'),
-  gutil = require('gulp-util');
+const fileinclude = require('gulp-file-include'),
+    gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    runSequence = require('run-sequence'),
+    sourcemaps = require('gulp-sourcemaps'),
+    babel = require('gulp-babel'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssnano = require('gulp-cssnano'),
+    gulpif = require('gulp-if');
 
- 
-gulp.task('fileinclude', function() {
-  gulp.src('views/**/*.html')
-    .pipe(fileinclude({
-      prefix: '@@',
-      basepath: 'includes'
-    }))
-    .pipe(gulp.dest('public'));
+gulp.task('default', ['dev', 'watch']);
+
+gulp.task('watch', function() {
+    global.prod = false;
+    gulp.watch('src/scripts/**/*.js', ['scripts']);
+    gulp.watch('src/styles/**/*.css', ['styles']);
+    gulp.watch(['src/views/**/*', 'src/includes/**/*', 'src/public/**/*'], ['files']);
 });
 
-gulp.task('run', ['fileinclude']);
-gulp.task('watch', function() {
-  gulp.watch('views/**/*.html', ['fileinclude']);
-  gulp.watch('includes/**/*', ['fileinclude']);
+gulp.task('prod', function () {
+    global.prod = true;
+    gulp.start('tasks');
+});
+gulp.task('dev', function () {
+    global.prod = false;
+    gulp.start('tasks');
+});
+gulp.task('tasks', ['scripts', 'styles', 'files']);
+
+gulp.task('scripts', function () {
+    return gulp.src(['core.js', 'validate.js', 'appstate.js', 'authentication.js',
+        'navbar.js', 'forumpostslist.js', 'forumpost.js', 'newpost.js', 'profile.js', 'signup.js'].map(p => `./src/scripts/${p}`), { base: './src/scripts' })
+        .pipe(babel())
+        .pipe(concat('main.bundle.min.js'))
+        .pipe(gulpif(global.prod, uglify()))
+        .pipe(gulp.dest('build/assets/scripts'));
+});
+
+gulp.task('styles', function () {
+    return gulp.src(['style.css', 'forumpostlist.css', 'forumpost.css', 'profile.css', 'signup.css']
+        .map(p => `src/styles/${p}`))
+        .pipe(gulpif(global.prod, autoprefixer()))
+        .pipe(concat('main.bundle.min.css'))
+        .pipe(gulpif(global.prod, cssnano()))
+        .pipe(gulp.dest('build/assets/styles'));
 })
 
-gulp.task('default', ['run', 'watch']);
+gulp.task('files', function () {
+    gulp.src('src/public/**/**/*').pipe(gulp.dest('build'))
+    gulp.src('src/views/**/**/*.html')
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: 'src/includes'
+        }))
+        .pipe(gulp.dest('build'));
+});
+
